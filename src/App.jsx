c900588aafa893bb6542ee7e9feb7e184d58a248
Anik Chowdhury <acdevilliersanik17@gmail.com>
2024-07-17 21:01:58 +0600

    Initial Change

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 import Card from './Components/card';
+import Header from './Components/header';
+import Footer from './Components/footer';
 
 function App() {
   return (
     <>
       <h1>Hello</h1>
 <Header />
-      <Card />
+    
       <Footer />
 
     </>
